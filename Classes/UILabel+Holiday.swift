ca7158fd5a4890f1d603d0c74748ed1922aa5da8
松永和男 <matsunaga@iarchitect.co.jp>
2019-01-29 00:31:11 +0900

    first commit

--- Classes/UILabel+Holiday.swift
+++ Classes/UILabel+Holiday.swift
@@ -11,9 +11,9 @@ public extension UIColor {
     convenience init(hex: String, alpha: CGFloat) {
         var hexString = hex
         if hexString.hasPrefix("#") {
-            hexString = String(hexString[hexString.index(hexString.startIndex, offsetBy: 1)...])
+            hexString = String(hexString[hexString.index(after: hexString.startIndex)..<hexString.endIndex])
         }
-        let value = hex.map { String($0) } + Array(repeating: "0", count: max(6 - hex.count, 0))
+        let value = hexString.map { String($0) } + Array(repeating: "0", count: max(6 - hexString.count, 0))
         let red = CGFloat(Int(value[0] + value[1], radix: 16) ?? 0) / 255.0
         let green = CGFloat(Int(value[2] + value[3], radix: 16) ?? 0) / 255.0
         let blue = CGFloat(Int(value[4] + value[5], radix: 16) ?? 0) / 255.0
